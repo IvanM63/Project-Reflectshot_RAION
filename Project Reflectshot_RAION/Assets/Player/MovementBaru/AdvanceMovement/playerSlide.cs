bc0fdd0345a2e51a14be381c2377bd6b01ada25f
IvanM63 <ivanmaulana.agusti@gmail.com>
2021-04-03 03:31:29 +0700

    Dash, Slide and Rocket
    knockback dari roket masih banyak bug

---
+++ Project Reflectshot_RAION/Assets/Player/MovementBaru/AdvanceMovement/playerSlide.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerSlide : MonoBehaviour
+{
+
+    public CharacterMovement player;
+
+    private Rigidbody2D rb;
+
+    public BoxCollider2D normalCollys;
+    public BoxCollider2D slideCollys;
+
+    public float slideSpeed;
+
+    private bool isSliding = false;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftShift)) {
+            Slide();
+        }
+    }
+
+    void Slide() {
+        isSliding = true;
+
+        normalCollys.enabled = false;
+        slideCollys.enabled = true;
+
+        if(player.facingRight) {
+            rb.AddForce(Vector2.right * slideSpeed);
+        } else {
+            rb.AddForce(Vector2.left * slideSpeed);
+        }
+        StartCoroutine(stopSliding());  
+    }
+
+    IEnumerator stopSliding() {
+
+        yield return new WaitForSeconds(0.5f);
+
+        normalCollys.enabled = true;
+        slideCollys.enabled = false;
+        isSliding = false;
+    }
+
+    public bool Sliding() {
+        return isSliding;
+    }
+    
+    public float getSlideSpeed() {
+        return slideSpeed;
+    }
+
+    void Slidd() {
+        isSliding = true;
+        if (player.facingRight) {
+            rb.AddForce(transform.right * slideSpeed);
+        } else {
+            rb.AddForce(transform.right * -slideSpeed);
+        }
+        isSliding = false;
+    }
+
+
+}
